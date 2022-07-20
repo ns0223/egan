5cf29b91a94a839639a2150a9682819c922cbd0c
ns <86961294+ns0223@users.noreply.github.com>
2022-07-20 23:45:23 +0530

    Update data_download.sh

--- data/data_download.sh
+++ data/data_download.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-mkdir DIV2K
-cd DIV2K
+mkdir DIV2k
+cd DIV2k
 
 # Training.
-echo "Downloading DIV2K train dataset."
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
-unzip DIV2K_train_HR.zip
-mv DIV2K_train_HR train
-rm DIV2K_train_HR.zip
+echo "Downloading DIV2k train dataset."
+wget http://data.vision.ee.ethz.ch/cvl/DIV2k/DIV2k_train_HR.zip
+unzip DIV2k_train_HR.zip
+mv DIV2k_train_HR train
+rm DIV2k_train_HR.zip
 
 # Validating.
-echo "Downloading DIV2K valid dataset."
+echo "Downloading DIV2k valid dataset."
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
-unzip DIV2K_valid_HR.zip
-mv DIV2K_valid_HR valid
-rm DIV2K_valid_HR.zip
+unzip DIV2k_valid_HR.zip
+mv DIV2k_valid_HR valid
+rm DIV2k_valid_HR.zip
